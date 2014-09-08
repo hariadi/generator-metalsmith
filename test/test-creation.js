2124d482b87a84e48f043479ec3d1c1464c88be7
Hariadi Hinta <diperakui@yahoo.com>
2014-09-08 19:10:49 +0800

    update yg and rewrite test

--- test/test-creation.js
+++ test/test-creation.js
@@ -1,24 +1,42 @@
 /*global describe, beforeEach, it */
 'use strict';
+
 var path = require('path');
+var assert = require('yeoman-generator').assert;
 var helpers = require('yeoman-generator').test;
 
 describe('metalsmith generator', function () {
-  beforeEach(function (done) {
-    helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
-      if (err) {
-        return done(err);
-      }
 
-      this.app = helpers.createGenerator('metalsmith:app', [
-        '../../app'
-      ]);
-      done();
-    }.bind(this));
+  before(function (done) {
+    helpers.run(path.join(__dirname, '../app'))
+      .inDir(path.join(__dirname, './temp'))
+      .withOptions({
+        'skip-install-message': true,
+        'skip-install': true,
+        'skip-welcome-message': true,
+        'skip-message': true
+      })
+      .withPrompt({
+        msTitle: 'Metalsmith Blog',
+        msDesc: 'My Metalsmith-Powered Site',
+        msAuthor: 'Metal Smith',
+        msGithubUser: 'metalsmith',
+        msPlugins: [
+          'metalsmith-ignore',
+          'metalsmith-drafts',
+          'metalsmith-templates',
+          'metalsmith-markdown',
+          'metalsmith-permalinks',
+          'metalsmith-collections'
+        ],
+        templateEngine: 'swig',
+        permalinksPattern: ':title',
+      })
+      .on('end', done);
   });
 
-  it('creates expected files', function (done) {
-    var expected = [
+  it('creates files', function () {
+    assert.file([
       '_layouts/post.html',
       '_posts/2012-08-20-first-post.md',
       '_posts/2012-08-23-second-post.md',
@@ -29,28 +47,7 @@ describe('metalsmith generator', function () {
       'metalsmith.json',
       'Makefile',
       'README.md'
-    ];
-
-    helpers.mockPrompt(this.app, {
-      msTitle: 'Metalsmith Blog',
-      msDesc: 'My Metalsmith-Powered Site',
-      msAuthor: 'Metal Smith',
-      msGithubUser: 'metalsmith',
-      msPlugins: [
-        'metalsmith-ignore',
-        'metalsmith-drafts',
-        'metalsmith-templates',
-        'metalsmith-markdown',
-        'metalsmith-permalinks',
-        'metalsmith-collections'
-      ],
-      templateEngine: 'swig',
-      permalinksPattern: ':title',
-    });
-    this.app.options['skip-install'] = true;
-    this.app.run({}, function () {
-      helpers.assertFile(expected);
-      done();
-    });
+    ]);
   });
+
 });
